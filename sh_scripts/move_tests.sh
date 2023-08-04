f5c91c7ea1e8cc74260c2255627d30ce54723a59
Brian Li <brnli7@gmail.com>
2023-08-04 11:54:09 -0400

    Print test.move

--- sh_scripts/move_tests.sh
+++ sh_scripts/move_tests.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+cat core_v2/sources/domain_e2e_tests.move
+
 echo "##### Running tests #####"
 ./aptos move test --package-dir core --named-addresses aptos_names=0x867ed1f6bf916171b1de3ee92849b8978b7d1b9e0a8cc982a3d19d535dfd9c0c,aptos_names_admin=0x91945b4672607a327019e768dd6045d1254d1102d882df434ca734250bb3581d,aptos_names_funds=0x78ee3915e67ef5d19fa91d1e05e60ae08751efd12ce58e23fc1109de87ea7865
 ./aptos move test --package-dir core_v2 --named-addresses aptos_names=0x867ed1f6bf916171b1de3ee92849b8978b7d1b9e0a8cc982a3d19d535dfd9c0c,aptos_names_v2=0x0ff30173389bf2bb7d101b715386c2f4bedbaf1438017cafa2da596e037681bc,aptos_names_admin=0x91945b4672607a327019e768dd6045d1254d1102d882df434ca734250bb3581d,aptos_names_funds=0x78ee3915e67ef5d19fa91d1e05e60ae08751efd12ce58e23fc1109de87ea7865
