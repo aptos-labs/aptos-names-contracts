52ac36d87d3a71577616bb91f4560fe253373025
Brian Li <brnli7@gmail.com>
2023-08-31 12:59:01 -0400

    Use ./aptos

--- sh_scripts/move_tests.sh
+++ sh_scripts/move_tests.sh
@@ -8,7 +8,7 @@ APTOS_NAMES_V2="0x867ed1f6bf916171b1de3ee92849b8978b7d1b9e0a8cc982a3d19d535dfd9c
 ADMIN="0x91945b4672607a327019e768dd6045d1254d1102d882df434ca734250bb3581d"
 FUNDS="0x78ee3915e67ef5d19fa91d1e05e60ae08751efd12ce58e23fc1109de87ea7865"
 ROUTER="0x867ed1f6bf916171b1de3ee92849b8978b7d1b9e0a8cc982a3d19d535dfd9c0c"
-ROUTER_SIGNER=0x$(aptos account derive-resource-account-address \
+ROUTER_SIGNER=0x$(./aptos account derive-resource-account-address \
   --address $ROUTER \
   --seed "ANS ROUTER" \
   --seed-encoding utf8 | \
