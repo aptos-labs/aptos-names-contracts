06775e9f586e6ca0dfdd14390edc8267220700bd
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2023-09-05 14:12:22 -0700

    fix dev setup

--- sh_scripts/move_tests.sh
+++ sh_scripts/move_tests.sh
@@ -3,4 +3,10 @@
 set -e
 
 echo "##### Running tests #####"
-./aptos move test --package-dir core --named-addresses aptos_names=0x867ed1f6bf916171b1de3ee92849b8978b7d1b9e0a8cc982a3d19d535dfd9c0c,aptos_names_admin=0x91945b4672607a327019e768dd6045d1254d1102d882df434ca734250bb3581d,aptos_names_funds=0x78ee3915e67ef5d19fa91d1e05e60ae08751efd12ce58e23fc1109de87ea7865
+APTOS_NAMES="0x867ed1f6bf916171b1de3ee92849b8978b7d1b9e0a8cc982a3d19d535dfd9c0c"
+ADMIN="0x91945b4672607a327019e768dd6045d1254d1102d882df434ca734250bb3581d"
+FUNDS="0x78ee3915e67ef5d19fa91d1e05e60ae08751efd12ce58e23fc1109de87ea7865"
+
+./aptos move test \
+  --package-dir core \
+  --named-addresses aptos_names=$APTOS_NAMES,aptos_names_admin=$ADMIN,aptos_names_funds=$FUNDS
