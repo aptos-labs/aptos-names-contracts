01e4c3ad946d99cacd80cf3ea5351979c8f9178b
0xaptosj <129789810+0xaptosj@users.noreply.github.com>
2023-09-21 14:59:27 -0700

    rename in script

--- sh_scripts/move_publish.sh
+++ sh_scripts/move_publish.sh
@@ -4,12 +4,12 @@ set -e
 
 echo "##### Publishing packages #####"
 # Set these to the account address you want to deploy to.
-APTOS_NAMES="0xc50ebbeba2335882faa281b22888776f1c5f098e5e3176f17374246508645e98"
-APTOS_NAMES_V2_1="0xc50ebbeba2335882faa281b22888776f1c5f098e5e3176f17374246508645e98"
-BULK="0xc50ebbeba2335882faa281b22888776f1c5f098e5e3176f17374246508645e98"
-ADMIN="0xc50ebbeba2335882faa281b22888776f1c5f098e5e3176f17374246508645e98"
-FUNDS="0xc50ebbeba2335882faa281b22888776f1c5f098e5e3176f17374246508645e98"
-ROUTER="0xc50ebbeba2335882faa281b22888776f1c5f098e5e3176f17374246508645e98"
+APTOS_NAMES="_"
+APTOS_NAMES_V2_1="_"
+BULK="_"
+ADMIN="_"
+FUNDS="_"
+ROUTER="_"
 
 ROUTER_SIGNER=0x$(aptos account derive-resource-account-address \
   --address $ROUTER \
@@ -19,18 +19,18 @@ ROUTER_SIGNER=0x$(aptos account derive-resource-account-address \
   sed -n 's/.*"Result": "\([^"]*\)".*/\1/p')
 
 aptos move publish \
-  --profile jianyi-test3 \
+  --profile core_profile \
   --package-dir core \
   --named-addresses aptos_names=$APTOS_NAMES,aptos_names_admin=$ADMIN,aptos_names_funds=$FUNDS,router_signer=$ROUTER_SIGNER
 aptos move publish \
-  --profile jianyi-test3 \
+  --profile core_v2_profile \
   --package-dir core_v2 \
   --named-addresses aptos_names=$APTOS_NAMES,aptos_names_v2_1=$APTOS_NAMES_V2_1,aptos_names_admin=$ADMIN,aptos_names_funds=$FUNDS,router=$ROUTER,router_signer=$ROUTER_SIGNER
 aptos move publish \
-  --profile jianyi-test3 \
+  --profile router_profile \
   --package-dir router \
   --named-addresses aptos_names=$APTOS_NAMES,aptos_names_v2_1=$APTOS_NAMES_V2_1,aptos_names_admin=$ADMIN,aptos_names_funds=$FUNDS,router=$ROUTER,router_signer=$ROUTER_SIGNER
 aptos move publish \
-  --profile jianyi-test3 \
+  --profile bulk_profile \
   --package-dir bulk \
   --named-addresses aptos_names=$APTOS_NAMES,aptos_names_v2_1=$APTOS_NAMES_V2_1,aptos_names_admin=$ADMIN,aptos_names_funds=$FUNDS,router=$ROUTER,router_signer=$ROUTER_SIGNER,bulk=$BULK
