fbca2cd5c99230de81e66939ffe4fd8fae0bf34d
Brian Li <brnli7@gmail.com>
2024-05-10 10:53:05 -0700

    Use version 3.3.0

--- sh_scripts/dev_setup.sh
+++ sh_scripts/dev_setup.sh
@@ -11,10 +11,8 @@ if ! command -v aptos &>/dev/null; then
     echo "aptos could not be found"
     echo "installing it..."
     TARGET=Ubuntu-x86_64
-    VERSION=2.0.2
+    VERSION=3.3.0
     wget https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v$VERSION/aptos-cli-$VERSION-$TARGET.zip
-    sha=$(shasum -a 256 aptos-cli-$VERSION-$TARGET.zip | awk '{ print $1 }')
-    [ "$sha" != "1f0ed0d0e042ff8b48b428eaaff9f52e6ff2b246a2054740d017f514c753c6cb" ] && echo "shasum mismatch" && exit 1
     unzip aptos-cli-$VERSION-$TARGET.zip
     chmod +x aptos
 else
