895a9dcfe8697b65d67a6e681950bc5d7991e020
Brian Li <brnli7@gmail.com>
2024-10-09 11:01:28 -0400

    Update aptos version (#189)

--- sh_scripts/dev_setup.sh
+++ sh_scripts/dev_setup.sh
@@ -11,7 +11,7 @@ if ! command -v aptos &>/dev/null; then
     echo "aptos could not be found"
     echo "installing it..."
     TARGET=Ubuntu-x86_64
-    VERSION=3.3.0
+    VERSION=4.2.1
     wget https://github.com/aptos-labs/aptos-core/releases/download/aptos-cli-v$VERSION/aptos-cli-$VERSION-$TARGET.zip
     unzip aptos-cli-$VERSION-$TARGET.zip
     chmod +x aptos
